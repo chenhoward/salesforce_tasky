c6ae764d8b183378759cb4bfadcb265e5bab1b64
Howard Chen <howardxchen@gmail.com>
2014-06-27 01:43:25 -0700

    Basic controller function

--- ProjectController.java
+++ ProjectController.java
@@ -2,7 +2,7 @@
 global class ProjectController{
 
     @RemoteAction
-    global static Tasky_Collaborator__c[] getCollaborators() {
+    global static Tasky_Collaborator__c[] getUserCollaborators() {
         Tasky_Collaborator__c[] collaborators = [SELECT Name, Project__c, Project__r.Name, Project__r.Description__c FROM Tasky_Collaborator__c WHERE User__c =: UserInfo.getUserId()];
         return collaborators;
     }
