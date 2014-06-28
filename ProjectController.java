e61ff5181b7539d4c5204bf1e14e0cc0b8119cdf
Howard Chen <howardxchen@gmail.com>
2014-06-27 19:36:56 -0700

    Create get task function

--- ProjectController.java
+++ ProjectController.java
@@ -1,9 +1,17 @@
 /** Controller for Tasky Projects Page. */
 global class ProjectController{
 
+    /** Gets all Collaborators associated with the User. */
     @RemoteAction
     global static Tasky_Collaborator__c[] getUserCollaborators() {
         Tasky_Collaborator__c[] collaborators = [SELECT Name, Project__c, Project__r.Name, Project__r.Description__c FROM Tasky_Collaborator__c WHERE User__c =: UserInfo.getUserId()];
         return collaborators;
+    }
+
+    /** Gets all Tasks associated with the Project with Id PROJECTID. */
+    @RemoteAction
+    global static Tasky_Task__c[] getTasks(Id projectId) {
+        Tasky_Task__c[] tasks = [SELECT Name, Assignee__c, Deadline__c, Detail__c FROM Tasky_Task__c WHERE Project__c =: projectId];
+        return tasks;
     }
 }
