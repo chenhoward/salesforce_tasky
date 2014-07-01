f31c3985d3dfef4bf040a07fe530e3b880533017
Howard Chen <howardxchen@gmail.com>
2014-07-01 12:20:19 -0700

    Add delete trigger

--- CollaboratorTrigger.java
+++ CollaboratorTrigger.java
@@ -1,4 +1,4 @@
-trigger Project_Share on Tasky_Collaborator__c (after insert, after delete) {
+trigger Project_Share on Tasky_Collaborator__c (after insert, before delete) {
 
     if (trigger.isInsert) {
         List<Tasky_Project__Share> projectShares = new List<Tasky_Project__Share>();
@@ -6,11 +6,18 @@ trigger Project_Share on Tasky_Collaborator__c (after insert, after delete) {
             Tasky_Project__Share collaboratorShare = new Tasky_Project__Share();
             collaboratorShare.ParentId = collaborator.Project__c;
             collaboratorShare.UserOrGroupId = collaborator.User__c;
-            collaboratorShare.AccessLevel = 'All';
+            collaboratorShare.AccessLevel = 'edit';
             collaboratorShare.RowCause = Schema.Tasky_Project__Share.RowCause.Collaborator_Access__c;
             projectShares.add(collaboratorShare);
         }
         Database.SaveResult[] projectShareInsertResult = Database.insert(projectShares,false);
-    }
+    } else if (trigger.isDelete) {
+        List<Tasky_Project__Share> projectShares = new List<Tasky_Project__Share>();
 
+        for (Tasky_Collaborator__c collaborator: trigger.old) {
+            Tasky_Project__Share[] collaboratorShares = [SELECT Id FROM Tasky_Project__Share WHERE ParentId =: collaborator.Project__c AND UserOrGroupId =: collaborator.User__c];
+            projectShares.add(collaboratorShares[0]);
+        }
+        Database.DeleteResult[] projectShareInsertResult = Database.delete(projectShares,false);
+    }
 }
