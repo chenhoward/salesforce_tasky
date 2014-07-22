46d40efaec6b84b940593d11a953ef059516c7cc
Howard Chen <howardxchen@gmail.com>
2014-07-22 10:04:32 -0700

    Fix Triggers

--- ProjectTrigger.java
+++ ProjectTrigger.java
@@ -1,12 +1,14 @@
 trigger Collaborator_Create on Tasky_Project__c (after insert) {
     if (trigger.isInsert) {
+        Tasky_Collaborator__c[] collaborators = new List<Tasky_Collaborator__c>();
         for (Tasky_Project__c proj: trigger.new){
             Tasky_Collaborator__c collaborator = new Tasky_Collaborator__c();
             collaborator.Project__c = proj.Id;
             collaborator.User__c = UserInfo.getUserId();
             collaborator.Name = UserInfo.getName();
-            insert collaborator;
+            collaborators.add(collaborator);
         }
+        insert collaborators;
     }
 }
 
@@ -23,10 +25,10 @@ trigger Task_Complete on Tasky_Task__c (before insert, before update) {
 trigger Task_Assign on Tasky_Task__c (before insert, before update) {
     if (trigger.isInsert || trigger.isUpdate) {
         for (Tasky_Task__c task: trigger.new){
-            Id owner = task.Owner__c;
-            Tasky_Collaborator__c[] collaborators = [SELECT Name FROM Tasky_Collaborator__c WHERE Project__c =: task.Project__c AND User__c =: task.Owner__c];
-            if (collaborators.size() == 0 && owner != null) {
-                task.addError('The User is not a collaborator in this project');
+            Id assignee = task.Assignee__c;
+            Tasky_Collaborator__c[] collaborators = [SELECT Name FROM Tasky_Collaborator__c WHERE Project__c =: task.Project__c AND User__c =: task.Assignee__c];
+            if (collaborators.size() == 0 && assignee != null) {
+                task.addError('User is not a collaborator in this project');
             }        
         }
     }
